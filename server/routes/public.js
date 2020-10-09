e2cd9ff150986f02f45c41748daad6e609f34de8
catai <catai@edu.uwaterloo.ca>
2020-10-09 13:28:34 -0400

    Set up stubbing endpoint to validate user.

--- server/routes/public.js
+++ server/routes/public.js
@@ -1,5 +1,16 @@
 const express = require('express')
 
+const authenticateHandler = require('../models/handlers/authenticate');
+
 const router = express.Router();
+
+router.get('/api/user-verification', async(req, res) => {
+    const email = req.query.email;
+    const password = req.query.password;
+
+    const response = await authenticateHandler.isUserCredentialsValid(email, password);
+
+    res.send(response);
+});
 
 module.exports = router
